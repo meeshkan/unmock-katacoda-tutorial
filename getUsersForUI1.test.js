83d9091c12e4e87028759073ddab536aeaee51f5
Mike Solomon <mike@meeshkan.com>
2019-09-17 16:23:04 +0300

    Adds tests

---
+++ getUsersForUI1.test.js
@@ -0,0 +1,30 @@
+const unmock = require("unmock");
+const getUsersForUI = require("./getUsersForUI");
+
+const { u } = unmock;
+
+unmock
+  .default
+  .nock("https://api.example.com/v1", "example")
+  .get("/users")
+  .reply(200, {
+    users: u.array(u.type({
+      id: u.number(),
+      name: u.string()
+    }, {
+      zodiac: u.type({
+        sign: u.string(),
+      }, {
+        ascendant: u.string()
+      })
+    }))
+  });
+
+beforeAll(() => {
+  unmock.default.on();
+});
+
+test("test get users for UI", async () => {
+  const usersForUI = await getUsersForUI();
+  expect(typeof usersForUI.timestamp).toBe("number");
+});
---
+++ getUsersForUI2.test.js
@@ -0,0 +1,49 @@
+const unmock = require("unmock");
+const getUsersForUI = require("./getUsersForUI");
+
+const {
+  u,
+  gen: { withCodes, responseBody },
+  runner
+} = unmock;
+
+unmock
+  .default
+  .nock("https://api.example.com/v1", "example")
+  .get("/users")
+  .reply(200, {
+    users: u.array(u.type({
+      id: u.number(),
+      name: u.string()
+    }, {
+      zodiac: u.type({
+        sign: u.string(),
+      }, {
+        ascendant: u.string()
+      })
+    }))
+  })
+  .get("/users")
+  .reply(404, {
+    message: "Service is down."
+  });
+
+let example;
+beforeAll(() => {
+  example = unmock.default.on().services.example;
+});
+
+test("usersForUI should always return a users array", runner(async () => {
+  const usersForUI = await getUsersForUI();
+  expect(usersForUI.users instanceof Array).toBe(true);
+}));
+
+test("usersForUI should initialize each user with seen: false", async () => {
+  example.state(
+    withCodes(200),
+    responseBody().minItems(1)
+  );
+  const usersForUI = await getUsersForUI();
+  expect(usersForUI.users.filter(i => i.seen === false).length)
+    .toBe(usersForUI.users.length);
+});
