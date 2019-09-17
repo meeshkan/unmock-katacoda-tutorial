c27ca4e3c7ee8b79d4b05af6d35266d4e7ea17cd
Mike Solomon <mike@meeshkan.com>
2019-09-17 16:15:49 +0300

    Adds tests

--- getUsersForUI.js
+++ getUsersForUI.js
@@ -1,2 +1,19 @@
-const getUsersForUI = () => 0;
+const axios = require("axios");
+
+const getUsersForUI = async () => {
+  try {
+    const { data } = await axios("https://api.example.com/v1/users");
+    return {
+      ...data,
+      users: data.users.map(user => ({ ...user, seen: false })),
+      newlyFetched: true,
+      timestamp: new Date().getTime()
+    };
+  } catch (e) {
+    return {
+      users: [],
+      error: e
+    }
+  }
+};
 module.exports = getUsersForUI;
--- getUsersForUI.test.js
+++
@@ -1,5 +0,0 @@
-const getUsersForUI = require("./getUsersForUI");
-
-test("test get users for UI", () => {
-  expect(getUsersForUI()).toBe(0);
-});
