385156f97dc9eafbe07c4563a7315919fd1fa0be
Fredrik Fornwall <fredrik@fornwall.net>
2020-01-07 13:57:42 +0100

    Make 3.test.js test that fields are added
    Also remove unused spreading of the users data.

--- users.js
+++ users.js
@@ -4,7 +4,6 @@ const getUsersForUI = async () => {
   try {
     const { data } = await axios("https://api.example.com/v1/users");
     return {
-      ...data,
       users: data.users.map(user => ({ ...user, seen: false })),
       newlyFetched: true,
       timestamp: new Date().getTime()
